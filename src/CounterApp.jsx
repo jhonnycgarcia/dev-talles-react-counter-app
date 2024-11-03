53573291beb539103e6182d8ce27b0332044e663
Jhonny <jhonnycgarcia@gmail.com>
2024-11-03 17:55:43 -0500

    useState - Hook

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
 export const CounterApp = ({ value }) => {
 
+    const [counter, setCount] = useState(value);
+
     const increment = () => {
         console.log('Clicked')
+        // setCount(counter + 1)
+        setCount((prevCount) => prevCount + 1)
     }
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> {value} </h2>
+            <h2> {counter} </h2>
             <button onClick={() => increment()}>
                 +1
             </button>
