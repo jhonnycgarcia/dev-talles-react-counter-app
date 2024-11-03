d13eab1d0856f40f15801ab2a23ad2252b1cbabd
Jhonny <jhonnycgarcia@gmail.com>
2024-11-03 18:01:42 -0500

    handleSubstract y handleReset

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -6,18 +6,25 @@ export const CounterApp = ({ value }) => {
     const [counter, setCount] = useState(value);
 
     const increment = () => {
-        console.log('Clicked')
         // setCount(counter + 1)
         setCount((prevCount) => prevCount + 1)
+    }
+
+    const decrement = () => {
+        setCount((prevCount) => prevCount - 1)
+    }
+
+    const reset = () => {
+        setCount(value)
     }
 
     return (
         <>
             <h1>CounterApp</h1>
             <h2> {counter} </h2>
-            <button onClick={() => increment()}>
-                +1
-            </button>
+            <button onClick={increment}>+1</button>
+            <button onClick={decrement}>-1</button>
+            <button onClick={reset}>Reset</button>
         </>
     );
 }
