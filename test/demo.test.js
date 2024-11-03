991ee09472aeb24354984efa3e63200e67d1da65
Jhonny <jhonnycgarcia@gmail.com>
2024-11-03 18:35:07 -0500

    Jest - Expect - toBe

--- test/demo.test.js
+++ test/demo.test.js
@@ -1,4 +1,9 @@
 
-test('Demo', () => {
 
+describe('Demo', () => {
+    test('Esta prueba no debe fallar', () => {
+        const message1  = 'Hola Mundo';
+        const message2 = message1.trim();
+        expect( message2 ).toBe( message1 );
+    });
 });
